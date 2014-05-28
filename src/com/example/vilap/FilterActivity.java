e933b7786a462ac1a772e847f5bbf5d2eed59ec7
Foad Saeidi Nik <f.saeidinik@gmail.com>
2014-05-28 17:41:08 +0430

    filter_activity

---
+++ src/com/example/vilap/FilterActivity.java
@@ -0,0 +1,12 @@
+package com.example.vilap;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class FilterActivity extends ActionBarActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.filter_activity);
+	}
+}
