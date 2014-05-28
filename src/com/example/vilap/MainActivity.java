c0634731dbadf106687c94cedcd2e28334660f7a
Foad Saeidi Nik <f.saeidinik@gmail.com>
2014-05-28 17:43:18 +0430

    commit

--- src/com/example/vilap/MainActivity.java
+++ src/com/example/vilap/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.vilap;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -12,6 +16,13 @@ public class MainActivity extends ActionBarActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		CustomAcitionBar.setActionBar(this);
+		ImageButton filterButton = (ImageButton) findViewById(R.id.actionbar_button_filter);
+		filterButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				startActivity(new Intent(MainActivity.this , FilterActivity.class));
+			}
+		});
 	}
 
 	@Override
